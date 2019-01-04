8e5a4a9b1553dea27d26ee4d77a1e459975f3df2
Vladyslav <itochanenko@gmail.com>
2019-01-05 00:55:00 +0200

    Tested TextView

--- playground/app/src/main/java/com/tochanenko/kotlinplayground/MainActivity.kt
+++ playground/app/src/main/java/com/tochanenko/kotlinplayground/MainActivity.kt
@@ -2,11 +2,18 @@ package com.tochanenko.kotlinplayground
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tv : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tv = findViewById(R.id.helloTextView)
+
+        tv.text = getString(R.string.hello)
     }
 }
