1d3278978f9609cb31d38a8bb4a7c533db75ae9d
Vladyslav <itochanenko@gmail.com>
2019-01-06 23:41:58 +0200

    Tested Button

--- playground/app/src/main/java/com/tochanenko/kotlinplayground/MainActivity.kt
+++ playground/app/src/main/java/com/tochanenko/kotlinplayground/MainActivity.kt
@@ -2,18 +2,24 @@ package com.tochanenko.kotlinplayground
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var tv : TextView
+    lateinit var button : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tv = findViewById(R.id.helloTextView)
+        button = findViewById(R.id.button)
 
         tv.text = getString(R.string.hello)
+        button.setOnClickListener {
+            tv.text = "Oh hello There!"
+        }
     }
 }
