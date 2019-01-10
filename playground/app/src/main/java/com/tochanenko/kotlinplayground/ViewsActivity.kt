3f81642ffd599aa983d8f2ad707e5b0b82436d55
Vladyslav <itochanenko@gmail.com>
2019-01-10 23:54:01 +0200

    Added activity with SeekBars

---
+++ playground/app/src/main/java/com/tochanenko/kotlinplayground/ViewsActivity.kt
@@ -0,0 +1,12 @@
+package com.tochanenko.kotlinplayground
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_views)
+    }
+}
