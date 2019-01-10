13584a250d15ba4c4f082a6eeb06119a46591212
Vladyslav <itochanenko@gmail.com>
2019-01-10 23:57:50 +0200

    Connected Kotlin to XML in SeekBar activity

--- playground/app/src/main/java/com/tochanenko/kotlinplayground/ViewsActivity.kt
+++ playground/app/src/main/java/com/tochanenko/kotlinplayground/ViewsActivity.kt
@@ -2,11 +2,23 @@ package com.tochanenko.kotlinplayground
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
 
 class ViewsActivity : AppCompatActivity() {
+
+    lateinit var seekBar : SeekBar
+    lateinit var seekBarDiscrete : SeekBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_views)
+
+        connectViews()
+    }
+
+    //  Connects Kotlin and XML
+    private fun connectViews() {
+        seekBar = findViewById(R.id.seekBar)
+        seekBarDiscrete = findViewById(R.id.seekBarDiscrete)
     }
 }
