efc8235763b39d57b884ada302333281d9431010
Vladyslav <itochanenko@gmail.com>
2019-02-07 23:57:46 +0200

    Now OK button works, fixed bug with number dividers

--- playground/app/src/main/java/com/tochanenko/kotlinplayground/MathActivity.kt
+++ playground/app/src/main/java/com/tochanenko/kotlinplayground/MathActivity.kt
@@ -3,11 +3,16 @@ package com.tochanenko.kotlinplayground
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import kotlin.random.Random
+import android.view.KeyEvent.KEYCODE_ENTER
+import android.view.View
+import android.view.inputmethod.EditorInfo
+
 
 class MathActivity : AppCompatActivity() {
 
@@ -38,22 +43,39 @@ class MathActivity : AppCompatActivity() {
         }
 
         mathActivitySubmit.setOnClickListener {
-            val str : String = mathActivityInputField.text.toString()
-            val selectedNumber : Int
-            var wrongNumber = false
-            if (str == "")
-                wrongNumber = true
-            else {
-                selectedNumber = str.toInt()
-                if ((selectedNumber in 2..number) && (number % selectedNumber == 0.toLong()))
-                    number /= selectedNumber
-                else
-                    wrongNumber = true
-            }
-            if (wrongNumber)
-                Toast.makeText(this, "Wrong number!", Toast.LENGTH_SHORT).show()
-            updateViews()
+            handleNumber()
         }
+
+        mathActivityInputField.setOnEditorActionListener() { v, actionId, event ->
+            if(actionId == EditorInfo.IME_ACTION_DONE){
+                handleNumber()
+                true
+            } else {
+                false
+            }
+        }
+    }
+
+    private fun handleNumber() {
+        val str : String = mathActivityInputField.text.toString()
+        val selectedNumber : Long
+        var wrongNumber = false
+        if (str == "")
+            wrongNumber = true
+        else {
+            selectedNumber = str.toLong()
+            if ((selectedNumber == 1.toLong()) || (selectedNumber == number)) {
+                Toast.makeText(this, "LOL! You can't fool me!", Toast.LENGTH_SHORT).show()
+                wrongNumber = true
+            }
+            else if ((selectedNumber in 2..(number - 1)) && (number % selectedNumber == 0.toLong()))
+                number /= selectedNumber
+            else
+                wrongNumber = true
+        }
+        if (wrongNumber)
+            Toast.makeText(this, "Wrong number!", Toast.LENGTH_SHORT).show()
+        updateViews()
     }
 
     private fun generateNumber() {
