e97596d8ccae77361052a5a29d621f41c2e9a4ee
Vladislav <itochanenko@gmail.com>
2019-03-04 10:57:37 +0200

    Tested HTML inside TextView

--- playground/app/src/main/java/com/tochanenko/kotlinplayground/TextTest.kt
+++ playground/app/src/main/java/com/tochanenko/kotlinplayground/TextTest.kt
@@ -1,8 +1,12 @@
 package com.tochanenko.kotlinplayground
 
+import android.annotation.SuppressLint
 import android.graphics.Typeface
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.annotation.RequiresApi
+import android.text.Html
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
@@ -10,10 +14,14 @@ import android.widget.TextView
 class TextTest : AppCompatActivity() {
 
     lateinit var tv : TextView
+    lateinit var strangeText : TextView
     lateinit var button : Button
     lateinit var checkBox : CheckBox
     var boldText : Boolean = false
 
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_test)
@@ -35,11 +43,14 @@ class TextTest : AppCompatActivity() {
                 tv.setTypeface(null, Typeface.BOLD)
             }
         }
+
+        strangeText.text = Html.fromHtml("<b>Hello</b> <i>it's me</i>", Html.FROM_HTML_MODE_COMPACT)
     }
 
     //  Connects Kotlin and XML
     private fun connectViews() {
         tv = findViewById(R.id.helloTextView)
+        strangeText = findViewById(R.id.strangeText)
         button = findViewById(R.id.button)
         checkBox = findViewById(R.id.checkBox)
     }
